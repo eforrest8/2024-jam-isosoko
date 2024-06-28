6346d13edbf5b8a2348798f51ef5835c41c8557a
Ethan Forrest <eforrest8@outlook.com>
2024-06-28 17:43:44 -0400

    Finished quad transform
    Now instead of drawing two triangles
    I can just draw one parallelogram,
    plus I get uv coordinates out of it.
    There might still be a more efficient
    way to do it, but this is fine for now.

--- src/isosoko/render2.nim
+++ src/isosoko/render2.nim
@@ -2,12 +2,12 @@ import globals
 import buffers
 import atomics
 import bitops
-import gamestate
 import math
 
 type
   Color = tuple[a, r, g, b: uint8]
   Vec2[T] = tuple[x, y: T]
+  Vec3[T] = tuple[x, y, z: T]
   TexPoint2d = Vec2[float]
   PixelPoint2d = Vec2[int]
   Rectangle = tuple[x, y, w, h: int]
@@ -26,14 +26,26 @@ func toARGB(self: Color): uint32 =
 proc toTexPoint2d(p: PixelPoint2d, bounds: Rectangle): TexPoint2d =
   return (x: float(p.x - bounds.x) / float(bounds.w), y: float(p.y - bounds.y) / float(bounds.h))
 
-func cross(v, w: Vec2[float]): float =
-  v.x * w.y - v.y * w.x
+func toVec3[T](v: Vec2[T]): Vec3[T] =
+  (x: v.x, y: v.y, z: 1)
+
+func cross(v, w: Vec2[float]): Vec2[float] =
+  (x: v.y - w.y, y: w.x - v.x)
+
+func cross(v, w: Vec3[float]): Vec3[float] =
+  (x: v.y*w.z - v.z*w.y, y: v.z*w.x - v.x*w.z, z: v.x*w.y - v.y*w.x)
 
 func dot(v, w: Vec2[float]): float =
   v.x * w.x + v.y * w.y
 
-func almostEqual(v, w: Vec2[float]): bool =
-  almostEqual(v.x, w.x) and almostEqual(v.y, w.y)
+func dot(v, w: Vec3[float]): float =
+  v.x * w.x + v.y * w.y + v.z * w.z
+
+func wedge(v, w: Vec2[float]): float =
+  v.x * w.y - v.y * w.x
+
+func almostEqual(v, w: Vec2[float], unitsInLastPlace: Natural = 4): bool =
+  almostEqual(v.x, w.x, unitsInLastPlace) and almostEqual(v.y, w.y, unitsInLastPlace)
 
 func abs[T](v: Vec2[T]): T =
   (x: abs v.x, y: abs v.y)
@@ -42,10 +54,13 @@ func magnitude[T](v: Vec2[T]): T =
   sqrt(v.x*v.x + v.y*v.y)
 
 func rotate[T](v: Vec2[T], theta: float): Vec2[T] =
-  (x: cos(theta)*v.x - sin(theta)*v.y, y: sin(theta)*v.x + cos(theta)*v.y)
+  (x: cos(theta)*v.x + sin(theta)*v.y, y: sin(theta)*v.x - cos(theta)*v.y)
 
 func shearX[T](v: Vec2[T], phi: float): Vec2[T] =
-  (x: cos(phi)*v.x - sin(phi)*v.y, y: v.y)
+  (x: v.x + ((cot(phi))*v.y), y: v.y)
+
+func scale2[T](v: Vec2[T], dx, dy: float): Vec2[T] =
+  (x: v.x * dx, y: v.y * dy)
 
 func `+`[T](v, w: Vec2[T]): Vec2[T] =
   (x: v.x + w.x, y: v.y + w.y)
@@ -62,11 +77,26 @@ func `*`[T](v, w: Vec2[T]): Vec2[T] =
 func `/`[T](v, w: Vec2[T]): Vec2[T] =
   (x: v.x / w.x, y: v.y / w.y)
 
+func `+`[T](v, w: Vec3[T]): Vec3[T] =
+  (x: v.x + w.x, y: v.y + w.y, z: v.z + w.z)
+
+func `-`[T](v, w: Vec3[T]): Vec3[T] =
+  (x: v.x - w.x, y: v.y - w.y, z: v.z - w.z)
+
+func `-`[T](v: Vec3[T]): Vec3[T] =
+  (x: -v.x, y: -v.y, z: -v.z)
+
+func `*`[T](v, w: Vec3[T]): Vec3[T] =
+  (x: v.x * w.x, y: v.y * w.y, z: v.z * w.z)
+
+func `/`[T](v, w: Vec3[T]): Vec3[T] =
+  (x: v.x / w.x, y: v.y / w.y, z: v.z / w.z)
+
 func within(p: Vec2[SomeNumber], b: Rectangle): bool =
   return p.x >= b.x and p.x < b.x + b.w and p.y >= b.y and p.y < b.y + b.h
 
 func withinUnit(p: Vec2[SomeNumber]): bool =
-  within(p, UNIT)
+  return p.x >= 0 and p.x < 1 and p.y >= 0 and p.y < 1
 
 func affineTransform(p: TexPoint2d, arg: AffineMatrix2d): TexPoint2d =
   return (x: p.x*arg[0] + p.y*arg[1] + arg[2], y: p.x*arg[3] + p.y*arg[4] + arg[5])
@@ -114,15 +144,15 @@ proc cleanupGlobals*(): void =
 
 func lineIntersection(p,r,q,s: Vec2[float]): bool =
   let qp = q-p
-  let rs = cross(r, s)
-  let qpr = qp.cross r
+  let rs = wedge(r, s)
+  let qpr = qp.wedge r
   if rs.almostEqual(0):
     if qpr.almostEqual(0):
       return true # lines are colinear
     else:
       return false # lines are parallel
   else:
-    let t = qp.cross(s)/rs
+    let t = qp.wedge(s)/rs
     let u = qpr/rs
     if 0 <= t and t <= 1 and 0 <= u and u <= 1:
       return true # lines intersect at p+tr=q+us
@@ -137,16 +167,27 @@ func isPointInTriangle(c1,c2,c3,p: Vec2[float]): bool =
   return 0 <= a and a <= 1 and 0 <= b and b <= 1 and 0 <= c and c <= 1
 
 func toParallelQuadSpace(p, origin, ver, hor, opposite: Vec2[SomeNumber]): Vec2[SomeNumber] =
-  # find the transforms which relate the quad to the unit square
-  # and then apply their inverse to the given point
-  # rotation of horizontal line relative to X axis
-  let theta = arccos(hor.x / hor.magnitude())
-  # difference in rotation between horizontal and vertical lines (shear angle)
-  let phi = arccos(ver.y / ver.magnitude()) - theta - (PI/4)
-  return (p - origin).rotate(-theta).shearX(-phi) / (opposite - origin)
+  #[
+    adapted from code in the following StackOverflow question:
+    https://gamedev.stackexchange.com/q/198925
+  ]#
+  let
+    P = origin.toVec3
+    M = hor.toVec3 - P
+    N = ver.toVec3 - P
+    A = P.cross N
+    B = M.cross P
+    C = N.cross M
+    S = p.toVec3
+    a = S.dot A
+    b = S.dot B
+    c = S.dot C
+    u = a / c
+    v = b / c
+  return (x: u, y: v)
 
 proc drawTile(p: PixelPoint2d): Color =
-  let theta = (PI) * (float(tick[]) / 2000)
+  let theta = (2*PI) * (float(tick[]) / 1000)
   let tp = toTexPoint2d(p, (x: 100, y: 80, w: 20, h: 20))
   let center: Vec2[float] = (x: 0, y: 0)
   let vecA: Vec2[float] = (x: sin(theta), y: -cos(theta))
@@ -154,15 +195,15 @@ proc drawTile(p: PixelPoint2d): Color =
   let vecC: Vec2[float] = (x: 0, y: 1)
   # segments 1&2, between A and B
   let corA = vecA + vecB
-  if isPointInTriangle(center, vecA, corA, tp) or isPointInTriangle(center, vecB, corA, tp):
+  if withinUnit(toParallelQuadSpace(tp, center, vecA, vecB, corA)):
     return (a: 255, r: 255, g: 0, b: 0)
   # segments 3&4, between B and C
   let corB = vecB + vecC
-  if isPointInTriangle(center, vecB, corB, tp) or isPointInTriangle(center, vecC, corB, tp):
+  if withinUnit(toParallelQuadSpace(tp, center, vecC, vecB, corB)):
     return (a: 255, r: 0, g: 255, b: 0)
   # segments 5&6, between C and A
   let corC = vecC + vecA
-  if isPointInTriangle(center, vecC, corC, tp) or isPointInTriangle(center, vecA, corC, tp):
+  if withinUnit(toParallelQuadSpace(tp, center, vecC, vecA, corC)):
     return (a: 255, r: 0, g: 0, b: 255)
   # point not in tile
   return (a: 255, r: 64, g: 64, b: 64)
@@ -178,3 +219,14 @@ proc drawScene*(): void =
     #pixels[][i] = toARGB((a: uint8 255, r: uint8 64, g: uint8 64, b: uint8 64))
     #pixels[][i] = uint32 0xffaaaaaa
   sren[].renderLock.clear()
+
+when isMainModule:
+  let epsilon = 1e-15
+  let sanityNear = toParallelQuadSpace((0.0,0.0), (0.0,0.0), (0.0,1.0), (1.0,0.0), (1.0,1.0))
+  let sanityHor = toParallelQuadSpace((1.0,0.0), (0.0,0.0), (0.0,1.0), (1.0,0.0), (1.0,1.0))
+  let sanityVer = toParallelQuadSpace((0.0,1.0), (0.0,0.0), (0.0,1.0), (1.0,0.0), (1.0,1.0))
+  let sanityFar = toParallelQuadSpace((1.0,1.0), (0.0,0.0), (0.0,1.0), (1.0,0.0), (1.0,1.0))
+  doAssert(abs(sanityNear.x - 0.0) < epsilon and abs(sanityNear.y - 0.0) < epsilon, $sanityNear)
+  doAssert(abs(sanityHor.x - 1.0) < epsilon and abs(sanityHor.y - 0.0) < epsilon, $sanityHor)
+  doAssert(abs(sanityVer.x - 0.0) < epsilon and abs(sanityVer.y - 1.0) < epsilon, $sanityVer)
+  doAssert(abs(sanityFar.x - 1.0) < epsilon and abs(sanityFar.y - 1.0) < epsilon, $sanityFar)
